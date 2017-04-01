f34975a1a5fc02a92be9e59b2c5553569eea90df
Maxime Daniel <root@maxux.net>
2017-04-02 00:29:42 +0200

    Simplify archive uncompressing process

--- src/archive.c
+++ src/archive.c
@@ -13,64 +13,16 @@
 #include "archive.h"
 #include "flister.h"
 
-#define CHUNK 0x4000
-#define ENABLE_ZLIB_GZIP 32
-
-//
-// uncompress source to destination
-//
-static int ungzip(FILE *source, FILE *destination) {
-	z_stream strm;
-	unsigned char in[CHUNK];
-	unsigned char out[CHUNK];
-
-	memset(&strm, 0, sizeof(z_stream));
-	strm.next_in = in;
-
-	// initializing inflate
-	if(inflateInit2(&strm, 15 | ENABLE_ZLIB_GZIP) != Z_OK)
-		return 1;
-
-	while (1) {
-		int bytes_read;
-
-		if((bytes_read = fread(in, sizeof(char), sizeof(in), source)))
-			if(ferror(source))
-				return 1;
-
-		strm.avail_in = bytes_read;
-
-		do {
-			unsigned have;
-			strm.avail_out = CHUNK;
-			strm.next_out = out;
-
-			if((inflate(&strm, Z_NO_FLUSH)) < 0)
-				return 1;
-
-			have = CHUNK - strm.avail_out;
-			fwrite(out, sizeof(unsigned char), have, destination);
-		}
-
-		while(strm.avail_out == 0);
-
-		if(feof(source)) {
-			inflateEnd(&strm);
-			break;
-		}
-	}
-
-	return 0;
-}
-
 //
 // uncompress gzip archive to the temporary directory
 //
 static char *archive_prepare(char *filename, char *target) {
 	char *destination;
-	FILE *src, *dst;
+	char buffer[4096];
+	FILE *dst;
+	gzFile gsrc;
 
-	if(!(src = fopen(filename, "r")))
+	if(!(gsrc = gzopen(filename, "r")))
 		diep(filename);
 
 	if(asprintf(&destination, "%s/%s", target, basename(filename)) < 0)
@@ -79,16 +31,11 @@ static char *archive_prepare(char *filename, char *target) {
 	if(!(dst = fopen(destination, "w")))
 		diep(destination);
 
-	// uncompress source to destination
-	printf("[+] uncompressing to: %s\n", destination);
-	if(ungzip(src, dst)) {
-		free(destination);
-		destination = NULL;
-		errno = ENOEXEC; // FIXME
-	}
+	while(gzread(gsrc, buffer, sizeof(buffer)))
+		fwrite(buffer, sizeof(buffer), 1, dst);
 
-	fclose(src);
 	fclose(dst);
+	gzclose(gsrc);
 
 	return destination;
 }
