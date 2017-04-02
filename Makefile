1e18e0a6046ee20751b0e074eeb7043c1cf17060
Maxime Daniel <root@maxux.net>
2017-04-02 15:21:54 +0200

    Improve Makefile, adding 'debug' target

--- Makefile
+++ Makefile
@@ -1,9 +1,13 @@
 default: all
 
+debug:
+	cd src && $(MAKE) $@
+	cp src/flister .
+
 all:
 	cd src && $(MAKE) $@
 	cp src/flister .
-	# strip -s flister
+	strip -s flister
 
 mrproper:
 	cd src && $(MAKE) $@
--- src/config.mk
+++
@@ -1,6 +0,0 @@
-EXEC = flister
-
-# CFLAGS  = -std=c99 -W -Wall -O2 -I/opt/rocksdb/include
-CFLAGS  = -std=c99 -W -Wall -O2 -g -I/opt/rocksdb/include
-# LDFLAGS = -static -pthread -ltar -lrt -lgflags -lz -ljemalloc -lb2 -lcapnp_c -L/opt/rocksdb -lrocksdb -llz4 -lbz2 -lz -lsnappy
-LDFLAGS = -pthread -ltar -lrt -lgflags -lz -ljemalloc -lb2 -lcapnp_c -L/opt/rocksdb -lrocksdb -llz4 -lbz2 -lz -lsnappy
