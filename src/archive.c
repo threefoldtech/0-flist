bd79c8dddf26c8d7a952551a4ee21efc88c544e9
Maxime Daniel <root@maxux.net>
2018-05-26 20:25:45 +0200

    Rewrite archive.c with right indentation

--- src/archive.c
+++ src/archive.c
@@ -17,57 +17,61 @@
 // uncompress gzip archive to the temporary directory
 //
 static char *archive_prepare(char *filename, char *target) {
-	char *destination;
-	char buffer[4096];
-	FILE *dst;
-	gzFile gsrc;
+    char *destination;
+    char buffer[4096];
+    FILE *dst;
+    gzFile gsrc;
 
-	if(!(gsrc = gzopen(filename, "r")))
-		diep(filename);
+    if(!(gsrc = gzopen(filename, "r")))
+        diep(filename);
 
-	if(asprintf(&destination, "%s/%s", target, basename(filename)) < 0)
-		diep("asprintf");
+    if(asprintf(&destination, "%s/%s", target, basename(filename)) < 0)
+        diep("asprintf");
 
-	if(!(dst = fopen(destination, "w")))
-		diep(destination);
+    if(!(dst = fopen(destination, "w")))
+        diep(destination);
 
-	while(gzread(gsrc, buffer, sizeof(buffer)))
-		fwrite(buffer, sizeof(buffer), 1, dst);
+    while(gzread(gsrc, buffer, sizeof(buffer)))
+        fwrite(buffer, sizeof(buffer), 1, dst);
 
-	fclose(dst);
-	gzclose(gsrc);
+    fclose(dst);
+    gzclose(gsrc);
 
-	return destination;
+    return destination;
 }
 
 char *archive_extract(char *filename, char *target) {
-	TAR *th = NULL;
-	struct stat st;
-	char *destination;
+    TAR *th = NULL;
+    struct stat st;
+    char *destination;
 
-	// checking if file is reachable
+    // checking if file is reachable
     if(stat(filename, &st))
-		return NULL;
+        return NULL;
 
-	// uncompression tar file to our ramdisk
-	verbose("[+] uncompressing archive: %s\n", filename);
-	if(!(destination = archive_prepare(filename, target)))
-		return NULL;
+    // uncompression tar file to our ramdisk
+    verbose("[+] uncompressing archive: %s\n", filename);
+    if(!(destination = archive_prepare(filename, target)))
+        return NULL;
 
-	// loading tar and extracting contents
-	verbose("[+] loading archive: %s\n", destination);
-	if(tar_open(&th, destination, NULL, O_RDONLY, 0644, TAR_GNU))
-		return NULL;
+    // loading tar and extracting contents
+    verbose("[+] loading archive: %s\n", destination);
+    if(tar_open(&th, destination, NULL, O_RDONLY, 0644, TAR_GNU))
+        return NULL;
 
-	// TODO: ensure security on files
+    // TODO: ensure security on files
 
-	verbose("[+] extracting archive\n");
-	if(tar_extract_all(th, target))
-		filename = NULL;
+    verbose("[+] extracting archive\n");
+    if(tar_extract_all(th, target))
+        filename = NULL;
 
-	tar_close(th);
-	free(destination);
+    tar_close(th);
+    free(destination);
 
-	return filename;
+    return filename;
 }
+/*
+char *archive_create(char *filename, char *source) {
 
+}
+*/
