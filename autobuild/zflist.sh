235ae751e57deb98337b3331b0d25e2e07e5781b
Maxime Daniel <root@maxux.net>
2021-02-15 22:16:51 +0100

    autobuild: switch to mbedtls

--- autobuild/zflist.sh
+++ autobuild/zflist.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 set -ex
 
-makeopts="-j 12"
+makeopts="-j $(grep ^flags /proc/cpuinfo | wc -l)"
 
 dependencies() {
     apt-get update
 
     apt-get install -y build-essential git libsnappy-dev libz-dev \
         libtar-dev libb2-dev autoconf libtool libjansson-dev \
-        libhiredis-dev libsqlite3-dev libssl-dev
+        libhiredis-dev libsqlite3-dev libssl-dev libmbedtls-dev
 }
 
 libcurl() {
@@ -21,7 +21,7 @@ libcurl() {
         --disable-telnet --disable-tftp --disable-pop3 --disable-imap --disable-smb --disable-smtp --disable-gopher \
         --disable-manual --disable-libcurl-option --disable-sspi --disable-ntlm-wb --without-brotli --without-librtmp --without-winidn \
         --disable-threaded-resolver \
-        --with-openssl
+        --without-openssl --with-mbedtls
 
     make ${makeopts}
     make install
@@ -53,7 +53,7 @@ zeroflist() {
     popd
 
     pushd zflist
-    make production
+    make production-mbed
     popd
 
     cp zflist/zflist /tmp/zflist
