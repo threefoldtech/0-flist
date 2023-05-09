f363bf5681cde9b7b480050b07d5937104df6616
Maxime Daniel <root@maxux.net>
2023-05-09 13:06:20 +0200

    libflist: fix another segmentation fault on localdir

--- libflist/flist_inode.c
+++ libflist/flist_inode.c
@@ -316,7 +316,8 @@ inode_t *flist_inode_from_localdir(char *localreldir, dirnode_t *parent, flist_c
     FTS* fs = NULL;
     FTSENT *fentry = NULL;
     char *ftsargv[2] = {localdir, NULL};
-    discard char *tmpsrc = dirname(strdup(localdir));
+    discard char *tmpsrcdup = strdup(localdir);
+    char *tmpsrc = dirname(tmpsrcdup);
     inode_t *inode = NULL;
     dirnode_t *workingdir = parent;
 
